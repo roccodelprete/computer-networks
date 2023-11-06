125a8cea9380d040fefc9884db8d253d02e77a7f
roccodelprete <122788299+roccodelprete@users.noreply.github.com>
2023-11-06 15:25:36 +0100

    move to visual studio code IDE and fix comments

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ computer networks notes and exercises
 
 # IDE
 Used CLion IDE.
+
+UPDATE 06/11/2023 – moved to visual studio code
+
+You can use both of them.
--- src/client-server/lib.h
+++ src/client-server/lib.h
@@ -2,8 +2,8 @@
 // Created by Rocco Del Prete on 23/10/23.
 //
 
-#ifndef CLIENT_SERVER_LIB_H
-#define CLIENT_SERVER_LIB_H
+#ifndef lib_h
+#define lib_h
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,9 +18,9 @@
 
 /**
  * function to create a socket
- * @param IPFamily – IP addresses family
- * @param type – stream type
- * @param protocol – protocol to use
+ * @param IPFamily IP addresses family
+ * @param type stream type
+ * @param protocol protocol to use
  * @return the descriptor created or exit if socket
  * method go in error
  */
@@ -29,8 +29,8 @@ int openSocket(int IPFamily, int type, int protocol);
 /**
  * function to associate port to network
  * using inet_pton
- * @param IPFamily – IP addresses family
- * @param argv – buffer arguments passed from terminal
+ * @param IPFamily IP addresses family
+ * @param argv buffer arguments passed from terminal
  * @param sin_addr – IP address
  */
 void portToNetwork(int IPFamily, char **argv, struct in_addr sin_addr);
@@ -38,60 +38,60 @@ void portToNetwork(int IPFamily, char **argv, struct in_addr sin_addr);
 /**
  * function to associate port to network
  * using inet_ntop
- * @param IPFamily – IP addresses family
- * @param argv – buffer arguments passed from terminal
- * @param sin_addr – IP address
+ * @param IPFamily IP addresses family
+ * @param argv buffer arguments passed from terminal
+ * @param sin_addr IP address
  */
 void portToNetworkNtop(int IPFamily, struct in_addr *sin_addr, char *buffer, socklen_t length);
 
 /**
  * function to connect socket to IP address
- * @param socketDescriptor – the socket
- * @param socketInfos – the socket infos (included IP address)
- * @param size – socket infos struct size
+ * @param socketDescriptor the socket
+ * @param socketInfos the socket infos (included IP address)
+ * @param size socket infos struct size
  */
 void connectToSocket(int socketDescriptor, struct sockaddr *socketInfos, int size);
 
 /**
  * function to assign address to socket
- * @param socketDescriptor – the socket
- * @param socketInfos – the socket infos
- * @param size – socket infos struct size
+ * @param socketDescriptor the socket
+ * @param socketInfos the socket infos
+ * @param size socket infos struct size
  */
 void assignAddress(int socketDescriptor, struct sockaddr *socketInfos, int size);
 
 /**
  * function to put a socket in listen
- * @param socketDescriptor – the socket
- * @param length – maximum connection that will be accepted
+ * @param socketDescriptor the socket
+ * @param length maximum connection that will be accepted
  */
 void socketInListenMode(int socketDescriptor, int length);
 
 /**
  * function to accept a connection
- * @param socketDescriptor – the socket
- * @param client – client who want the connection
- * @param addressSize – client address size
+ * @param socketDescriptor the socket
+ * @param client client who want the connection
+ * @param addressSize client address size
  * @return the connection accepted
  */
 int acceptConnection(int socketDescriptor, struct sockaddr *client, socklen_t *addressSize);
 
 /**
  * function to write on a socket
- * @param fileDescriptor – where to write
- * @param buffer – what to write
- * @param count – how many bytes to write
+ * @param fileDescriptor where to write
+ * @param buffer what to write
+ * @param count how many bytes to write
  * @return the number bytes written
  */
 ssize_t fullWrite(int fileDescriptor, const void *buffer, size_t count);
 
 /**
  * function to read from a socket
- * @param fileDescriptor – where to read
- * @param buffer – what to read
- * @param count – how many bytes to read
+ * @param fileDescriptor where to read
+ * @param buffer what to read
+ * @param count how many bytes to read
  * @return the number bytes read
  */
 ssize_t fullRead(int fileDescriptor, void *buffer, size_t count);
 
-#endif //CLIENT_SERVER_LIB_H
+#endif
